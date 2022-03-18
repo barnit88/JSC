13bf0442168bf26bac5d1614dfa881416e39e750
Barnit Basnet <barnit.basnet1@gmail.com>
2022-03-18 23:36:55 +0545

    Delete operator in JavaScript

--- DeleteOperator.js
+++ DeleteOperator.js
@@ -8,8 +8,12 @@
  * is returned
  */
 
-//  console.log(objectLiteral.param1);
-
+/**
+ * If you are a JavaScript developer you might have seen delete keyword.
+ * Delete operator of JavaScript allows us to delete an object property.
+ * Delete operator takes the object property and removes the object property from 
+ * the object. 
+ */
 var objectLiteral = {
     
     param2 : "Param 2",
@@ -22,10 +26,33 @@ console.log(objectLiteral.param1);
 objectLiteral.param1 = function() {
     console.log("Example");
 }
+function Person(firstName , lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
+
+var barnit = new Person("Barnit", "Basnet");
+console.log(barnit);
+// delete barnit.firstName;
+delete barnit;
+console.log(barnit);
 
 
-console.log(objectLiteral);
-objectLiteral.param1();
+// console.log(delete Person);
 
-delete objectLiteral.param1;
-console.log(objectLiteral);
+// console.log(objectLiteral);
+// objectLiteral.param1();
+
+// var isDeleted = delete objectLiteral.param1;
+// console.log(isDeleted);
+// console.log(objectLiteral);
+
+
+// var deleteMe = "Delete Me";
+// console.log(delete deleteMe);
+// function DeleteMeFunction(){
+
+// }
+// console.log(delete window.DeleteMeFunction);
+// ddd = "Hey Delete";
+// console.log(delete ddd);
